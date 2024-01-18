78bf8f11f70efc9a26329e3164da152673b93178
piratejas <piratejas@gmail.com>
2024-01-18 14:49:59 +0000

    display list of heroes using ngFor; add conditional rendering of hero details using ngIf and event listener on button element

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
+	heroes = HEROES;
+	selectedHero?: Hero;
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
 	}
 }
