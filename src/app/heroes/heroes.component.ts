1c763504f805d542e357ab8df5bd46c711b6b911
piratejas <piratejas@gmail.com>
2024-01-18 16:48:05 +0000

    refactored to add HeroService using async Observable; added MessageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,10 +13,19 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css'
 })
 
-export class HeroesComponent {
-	heroes = HEROES;
+export class HeroesComponent implements OnInit {
+	heroes: Hero[] = [];
 	selectedHero?: Hero;
+	constructor(private heroService: HeroService, private messageService: MessageService) {}
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+		this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+	}
+	getHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes);
 	}
 }
